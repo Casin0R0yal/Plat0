039ada730a7ed3d804fecfc80d9582c16854bd0c
Khaled MILI <khaled.mili@epita.fr>
2023-03-22 16:45:42 +0100

    need to do game processing and gui

--- Connect4/Board.java
+++ Connect4/Board.java
@@ -3,9 +3,9 @@ package Connect4;
 
 
 public class Board{
-    int rows = 7;
-    int cols = 6;
-    int[][] table = new int[7][6];
+    public const int rows = 7;
+    public const int cols = 6;
+    public int[][] table = new int[7][6];
 
     public Board()
     {
@@ -13,16 +13,45 @@ public class Board{
 
     public static boolean IsEmpty()
     {
-        return false;
+        for(int i = 0; i < cols; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                if(table[j][i] != 0)
+                    return false;
+            }
+        }
+        return true;
+
     }
 
     public static boolean IsFull()
     {
-        return false;
+        for(int i = 0; i < cols; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                if(table[j][i] == 0)
+                    return false;
+            }
+        }
+        return true;
     }
 
     public static void Clear()
     {
+        for(int i = 0; i < cols; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                table[j][i] = 0;
+            }
+        }
+    }
+
+    public static void PutCell(Player p, int x, int y)
+    {
+        table[x][y] = p.token;
     }
 
     public void PrettyPrint()
--- Connect4/Player.java
+++ Connect4/Player.java
@@ -3,13 +3,16 @@ package Connect4;
 
 public class Player(){
 
-    String color;
-    boolean HasWon;
+    public String color;
+    public int token;
+    public boolean HasWon;
 
-    public Player(String color)
+    public Player(String color,int token)
     {
         this.color = color;
+        this.token = token;
         Haswon = false;
+
     }
 
 }
