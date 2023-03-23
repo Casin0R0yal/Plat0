6375e6b95bf0a42169f7166af69f136342c211a6
romain.dupre <romain.dupre@epita.fr>
2023-03-23 13:33:57 +0000

    on va tout changer

--- ProjMonopoly/Proj/Affichage.java
+++ ProjMonopoly/Proj/Affichage.java
@@ -13,19 +13,20 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 class Affichage extends JFrame {
-    public static final int WINDOW_WIDTH = 800;
-    private static final int WINDOW_HEIGHT = 600;
+    public static final int WINDOW_WIDTH = 1000;
+    private static final int WINDOW_HEIGHT = 800;
     private Image image;
     private Image player1;
     private Image player2;
     private Image player3;
-    private Image player4;
+    private Image player4;    
+
     private int imageWidth;
     private int imageHeight;
     private int playerheight;
     private int playerwidth;
-    int x = 730;
-    int y = 520;
+    int x = 780;
+    int y = 610;
     int playerId = 0;
     int playerCount = 0;
     JButton playButton;
@@ -34,7 +35,12 @@ class Affichage extends JFrame {
     JLabel playerLabel2;
     JLabel playerLabel3;
     JLabel playerLabel4;
-    boolean buy = false;
+
+    private String all;
+    private JLabel list1;
+    private JLabel list2;
+    private JLabel list3;
+    private JLabel list4;
 
     // Create a JPanel to display the image
     JPanel panel = new JPanel() {
@@ -60,13 +66,14 @@ class Affichage extends JFrame {
         setResizable(false);
 
         // Load the image from a file
-        image = Toolkit.getDefaultToolkit().getImage("image.jpg");
+        image = Toolkit.getDefaultToolkit().getImage("monopoly.png");
         imageWidth = image.getWidth(null);
         imageHeight = image.getHeight(null);
 
         playerwidth = 30;
         playerheight = 30;
-        
+
+        all = "player"+playerId+" case/nb of house/price: ";
         // Create a button and add it to the JPanel
         playButton = new JButton("Play");
         panel.add(playButton);
@@ -79,24 +86,32 @@ class Affichage extends JFrame {
             player1 = player1.getScaledInstance(playerwidth, playerheight, Image.SCALE_SMOOTH);
             playerLabel1 = new JLabel(new ImageIcon(player1));
             panel.add(playerLabel1);
+            list1 = new JLabel("player1 case/nb of house/price: ");
+            panel.add(list1);
         }
         if (Server.PLAYER_MAX > 1) {
             player2 = Toolkit.getDefaultToolkit().getImage("player.png");
             player2 = player2.getScaledInstance(playerwidth, playerheight, Image.SCALE_SMOOTH);
             playerLabel2 = new JLabel(new ImageIcon(player2));
             panel.add(playerLabel2);
+            list2 = new JLabel("player2 case/nb of house/price: ");
+            panel.add(list2);
         }
         if (Server.PLAYER_MAX > 2) {
             player3 = Toolkit.getDefaultToolkit().getImage("player.png");
             player3 = player3.getScaledInstance(playerwidth, playerheight, Image.SCALE_SMOOTH);
             playerLabel3 = new JLabel(new ImageIcon(player3));
             panel.add(playerLabel3);
+            list3 = new JLabel("player3 case/nb of house/price: ");
+            panel.add(list3);
         }
         if (Server.PLAYER_MAX > 3) {
             player4 = Toolkit.getDefaultToolkit().getImage("player.png");
             player4 = player4.getScaledInstance(playerwidth, playerheight, Image.SCALE_SMOOTH);
             playerLabel4 = new JLabel(new ImageIcon(player4));
             panel.add(playerLabel4);
+            list4 = new JLabel("player4 case/nb of house/price: ");
+            panel.add(list4);
         }
         
         // Set the position of the button to the center of the JPanel
@@ -116,16 +131,20 @@ class Affichage extends JFrame {
                 playButton.setBounds(panel.getWidth()/2 - 50, panel.getHeight()/2 - 25, 100, 50);
                 Buy.setBounds(panel.getWidth()/2 - 50, panel.getHeight()/2 - 25, 0, 0);
                 if (Server.PLAYER_MAX > 0) {
+                    list1.setBounds(170, 670, 500, 20);
                     playerLabel1.setBounds(x, y, playerwidth, playerheight);
                 }
                 if (Server.PLAYER_MAX > 1) {
-                    playerLabel2.setBounds(x, y, playerwidth, playerheight);
+                    list2.setBounds(170, 690, 500, 20);
+                    playerLabel2.setBounds(x+5, y, playerwidth, playerheight);
                 }
                 if (Server.PLAYER_MAX > 2) {
-                    playerLabel3.setBounds(x, y, playerwidth, playerheight);
+                    list3.setBounds(170, 20, 500, 20);
+                    playerLabel3.setBounds(x, y-5, playerwidth, playerheight);
                 }
                 if (Server.PLAYER_MAX > 3) {
-                    playerLabel4.setBounds(x, y, playerwidth, playerheight);
+                    list4.setBounds(170, 40, 500, 20);
+                    playerLabel4.setBounds(x+5, y-5, playerwidth, playerheight);
                 }
                 repaint();
             }
@@ -164,6 +183,22 @@ class Affichage extends JFrame {
                 playButton.setBounds(panel.getWidth()/2 - 50, panel.getHeight()/2 - 25, 100, 50);
                 Buy.setBounds(panel.getWidth()/2 - 50, panel.getHeight()/2 - 25, 0, 0);
                 Server.currentPlayer = (Server.currentPlayer + 1) % playerCount;
+                for (Case c : Board.cases) {
+                    if (c.position == Server.positions.get(playerId)) {
+                        if (c.owner == null) {
+                            c.owner = Server.players.get(playerId);
+                            System.out.println("You bought " + c.name);
+                            AddCase(c);
+                            Server.players.get(playerId).number += 1;
+                        }
+                        else if (c.owner == Server.players.get(playerId)) {
+                            System.out.println("You already own this property");
+                        }
+                        else {
+                            System.out.println("This property is already owned by another player");
+                        }
+                    }
+                }
             }
         });
 
@@ -176,26 +211,45 @@ class Affichage extends JFrame {
         int pos = Server.positions.get(playerId);
         int val = 0;
         if (pos != 0 && pos != 10 && pos != 20 && pos != 30) {
-            val = 25;
+            val = 10;
             if (pos > 0 % 20 && pos < 10 % 20) {
                 val += 10;
             }
         }
         if (pos < 10) {
-            x = 750 - pos * 66 - val;
-            y = 520;
+            x = 780 - pos * 55 - val;
+            y = 610;
         }
         else if (pos < 20) {
-            x = 30;
-            y = 520 - (pos - 10) * 46 - val;
+            x = 200;
+            y = 610 - (pos - 10) * 48 - val;
         }
         else if (pos < 30) {
-            x = 30 + (pos - 20) * 66 + val;
-            y = 20;
+            x = 200 + (pos - 20) * 55 + val;
+            y = 110;
         }
         else if (pos < 40) {
-            x = 750;
-            y = 10 + (pos - 30) * 46 + val;
+            x = 780;
+            y = 110 + (pos - 30) * 48 + val;
+        }
+    }
+
+    //Draw the case of the player
+    public void AddCase(Case c) {
+        all += " n"+c.position+"/0/"+c.price;
+        for (Player player : Server.players) {
+            if (playerId == 0) {
+                player.affichage.list1.setText(all);
+            }
+            else if (playerId == 1) {
+                player.affichage.list2.setText(all);
+            }
+            else if (playerId == 2) {
+                player.affichage.list3.setText(all);
+            }
+            else if (playerId == 3) {
+                player.affichage.list4.setText(all);
+            }
         }
     }
 
@@ -205,13 +259,13 @@ class Affichage extends JFrame {
             player.affichage.playerLabel1.setBounds(x, y, playerwidth, playerheight);
         }
         else if (playerId == 1) {
-            player.affichage.playerLabel2.setBounds(x, y, playerwidth, playerheight);
+            player.affichage.playerLabel2.setBounds(x+5, y, playerwidth, playerheight);
         }
         else if (playerId == 2) {
-            player.affichage.playerLabel3.setBounds(x, y, playerwidth, playerheight);
+            player.affichage.playerLabel3.setBounds(x, y-5, playerwidth, playerheight);
         }
         else if (playerId == 3) {
-            player.affichage.playerLabel4.setBounds(x, y, playerwidth, playerheight);
+            player.affichage.playerLabel4.setBounds(x+5, y-5, playerwidth, playerheight);
         }
     }
 }
---
+++ ProjMonopoly/Proj/Board.java
@@ -0,0 +1,71 @@
+package Proj;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+class Case {
+    public int price;
+    public String name;
+    public Player owner = null;
+    public int position;
+    public String path;
+
+    public Case(int price, String name, int position, String path) {
+        this.price = price;
+        this.name = name;
+        this.position = position;
+        this.path = path;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+}
+
+public class Board {
+    public static List<Case> cases = new ArrayList<Case>();
+    public Board() {
+        cases.add(new Case(0, "Départ", 0, null));
+        cases.add(new Case(60, "Boulevard de Belleville", 1, "image1.png"));
+        cases.add(new Case(0, "Caisse de Communauté", 2, null));
+        cases.add(new Case(60, "Rue Lecourbe", 3, "image3.png"));
+        cases.add(new Case(200, "Impôts sur le revenu", 4, null));
+        cases.add(new Case(200, "Gare Montparnasse", 5, "image5.png"));
+        cases.add(new Case(100, "Rue de Vaugirard", 6, "image6.png"));
+        cases.add(new Case(0, "Chance", 7, null));
+        cases.add(new Case(100, "Rue de Courcelles", 8, "image8.png"));
+        cases.add(new Case(120, "Avenue de la République", 9, "image9.png"));
+        cases.add(new Case(0, "Prison", 10, null));
+        cases.add(new Case(140, "Boulevard de la Villette", 11, "image11.png"));
+        cases.add(new Case(150, "Compagnie de distribution d'électricité", 12, "image12.png"));
+        cases.add(new Case(140, "Avenue de Neuilly", 13, "image13.png"));
+        cases.add(new Case(160, "Rue de Paradis", 14, "image14.png"));
+        cases.add(new Case(200, "Gare de Lyon", 15, "image15.png"));
+        cases.add(new Case(180, "Avenue Mozart", 16, "image16.png"));
+        cases.add(new Case(0, "Caisse de Communauté", 17, null));
+        cases.add(new Case(180, "Boulevard Saint-Michel", 18, "image18.png"));
+        cases.add(new Case(200, "Place Pigalle", 19, "image19.png"));
+        cases.add(new Case(0, "Aller en prison", 20, null));
+        cases.add(new Case(220, "Avenue Matignon", 21, "image21.png"));
+        cases.add(new Case(0, "Chance", 22, null));
+        cases.add(new Case(220, "Boulevard Malesherbes", 23, "image23.png"));
+        cases.add(new Case(240, "Avenue Henri-Martin", 24, "image24.png"));
+        cases.add(new Case(200, "Gare du Nord", 25, "image25.png"));
+        cases.add(new Case(260, "Faubourg Saint-Honoré", 26, "image26.png"));
+        cases.add(new Case(260, "Place de la Bourse", 27, "image27.png"));
+        cases.add(new Case(150, "Compagnie de distribution des eaux", 28, "image28.png"));
+        cases.add(new Case(280, "Rue de la Fayette", 29, "image29.png"));
+        cases.add(new Case(0, "Parking gratuit", 30, null));
+        cases.add(new Case(300, "Avenue de Breteuil", 31, "image31.png"));
+        cases.add(new Case(300, "Avenue Foch", 32, "image32.png"));
+        cases.add(new Case(0, "Caisse de Communauté", 33, null));
+        cases.add(new Case(320, "Boulevard des Capucines", 34, "image34.png"));
+        cases.add(new Case(200, "Gare Saint-Lazare", 35, "image35.png"));
+        cases.add(new Case(0, "Chance", 36, null));
+        cases.add(new Case(350, "Avenue des Champs-Élysées", 37, "image37.png"));
+        cases.add(new Case(100, "Taxe de luxe", 38, null));
+        cases.add(new Case(400, "Rue de la Paix", 39, "image39.png"));
+    }
+}
