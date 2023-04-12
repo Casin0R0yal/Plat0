1333d99c02c905d19f600a5a0f6e53b88c237713
Khaled MILI <khaled.mili@epita.fr>
2023-04-12 14:11:39 +0200

    UI working

---
+++ Connect4/Connect4GUI.java
@@ -0,0 +1,98 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.geom.Ellipse2D;
+
+public class Connect4GUI extends JFrame implements ActionListener {
+
+    private Board board;
+    private JButton[][] grid;
+    private JLabel playerTurnLabel;
+    private Game game;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+
+    public Connect4GUI() {
+        board = new Board();
+        game = new Game(board);
+
+        player1 = new Player("red", 1);
+        player2 = new Player("yellow", 2);
+        currentPlayer = player1;
+
+        setTitle("Connect 4");
+        setSize(700, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        // Create the board grid
+        JPanel boardPanel = new JPanel(new GridLayout(6, 7));
+        grid = new JButton[6][7];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                grid[i][j] = new JButton();
+                grid[i][j].setPreferredSize(new Dimension(80, 80));
+                grid[i][j].addActionListener(this);
+                boardPanel.add(grid[i][j]);
+            }
+        }
+
+        // Create the player turn label
+        playerTurnLabel = new JLabel(currentPlayer.getName() + "'s turn");
+
+        // Add the board and player turn label to the frame
+        add(boardPanel, BorderLayout.CENTER);
+        add(playerTurnLabel, BorderLayout.NORTH);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // Get the button that was clicked
+        JButton button = (JButton) e.getSource();
+
+        // Find the row and column of the button in the grid
+        int row = -1;
+        int col = -1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (grid[i][j] == button) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+
+        // Check if the move is valid
+        int[] putResult = board.PutCell(currentPlayer, col);
+        if (putResult == null) {
+            return;
+        }
+
+        // Update the grid and check for a winner
+        grid[putResult[0]][putResult[1]].setBackground(currentPlayer.getColor());
+        if (game.EndGame()) {
+            JOptionPane.showMessageDialog(this, currentPlayer.getName() + " has won!");
+            return;
+        }
+        if (game.TieGame()) {
+            JOptionPane.showMessageDialog(this, "The game is a tie.");
+            return;
+        }
+
+        // Switch to the other player's turn
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+        playerTurnLabel.setText(currentPlayer.getName() + "'s turn");
+    }
+
+    public static void main(String[] args) {
+        new Connect4GUI();
+    }
+
+}
--- Connect4/Player.java
+++ Connect4/Player.java
@@ -1,16 +1,31 @@
 import java.io.*;
-
+import java.awt.Color;
 public class Player {
 
     public String color;
     public int token;
+    public boolean IsPlaying;
     public boolean HasWon;
 
     public Player(String color,int token)
     {
         this.color = color;
         this.token = token;
+        IsPlaying = false;
         HasWon = false;
+    }
+
+    public Color getColor()
+    {
+        if(color == "red")
+            return Color.RED;
+        else
+            return Color.YELLOW;
+    }
+
+    public String getName()
+    {
+        return color;
     }
 
 }
